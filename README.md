0944f2916d796ca108df1df3acd5ebcde90851ef
Mr Mike <hemontemayor.c02@ucamp.utel.edu.mx>
2021-08-25 21:38:02 -0500

    cambios en JS y el readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # intro-JS
 iniciando de a de veras con java script
+
+
+
+const Persona 1 =  'Mike' 
+const Persona 2 =  'Mike'
+const Persona 3 =  'Mike'
--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-console.log("Hola mundo");
+
+
+
+
+console.log("Hola mundo");   //console.log es para imprimir los valores de las variables, string o cualquier otr funcion
+
+var nombre;
+ console.log(nombre); // Var Es un valor que cambia
+
+ nombre = 'Mike';
+ console.log(nombre);
+ 
+ nombre = 'Joy';
+ console.log(nombre);
+
+ nombre = 'Maika';
+ console.log (nombre);
+
+ nombre = "otro nombre";
+ console.log(nombre);
+
+ console.log(-(10 -2))
+
+ let elNumero = Number(prompt("Elige un numero")); //tengo que investigar las diferencias entre var y let
+ console.log("Tu numero es la raiz cuadrada de " + elNumero * elNumero);
+
+
+ const id = 1647; // Es una variable con un valor que no se puede cambiar
+ console.log(id);
+
+
+  var nombre = 'miguel'
+  var edad = 35;
+  const fechaDeNacimiento = " nacio el 11 de diciembre de 1989";
+  const LugarDeNacimiento = "Edo. de Mexico"
+  console.log(nombre,fechaDeNacimiento,LugarDeNacimiento);
+ 
