d115a5df7144c9aa2112429b39e711dc34b00b2a
Mr Mike <hemontemayor.c02@ucamp.utel.edu.mx>
2021-08-29 05:05:19 -0500

    Ejercicios

---
+++ ejercicio.js
@@ -0,0 +1,22 @@
+
+
+function pares(max){
+var num = 2;
+for( var i = 1; i <= 6; i++){
+    var b = num*1
+    console.log(b);
+    }
+}
+pares(12);
+pares(3);
+pares(100);
+
+
+//imprimir un alert con un nombre
+alert('Pedro GOnzalez')
+
+//Crear un metodo que reciba tu nombre y tu edad y determine si eres mayor de edad para solicitar un permiso de conducir
+
+permiso("Roberto",15)//"Roberto, no puedes solicitar el permiso"
+permiso("Mario",36)//"MArio, puedes solicitar tu permiso"
+//se puede usar las condicionales if/else
